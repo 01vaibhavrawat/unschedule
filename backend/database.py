ef8fdfd4fea469f9098bfd4570fb7e4893002306
Vaibhav Rawat <vaibhrawat@gmail.com>
2026-04-07 12:23:18 +0530

    added gitignore file, configured mongodb

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://smartwire:smartwire@etd-shard-00-00.nvgep.mongodb.net:27017,etd-shard-00-01.nvgep.mongodb.net:27017,etd-shard-00-02.nvgep.mongodb.net:27017/?replicaSet=atlas-rciogd-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=ETD")
 client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.unschedule
 
